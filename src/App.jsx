5ae64aec98575f5307125c3cf177539b50196073
crowtek <prt3@hotmail.de>
2024-04-07 18:13:51 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar";
+import NewProject from "./components/NewProject";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8 ">
+      <Sidebar />
+      <NewProject />
+    </main>
+  );
+}
+
+export default App;
